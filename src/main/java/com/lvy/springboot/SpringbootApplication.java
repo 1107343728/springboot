7941947810a6fbe36e626039c361e60de16f828f
1107343728 <1107343728@qq.com>
2018-09-24 17:37:04 +0800

    test

--- src/main/java/com/lvy/springboot/SpringbootApplication.java
+++ src/main/java/com/lvy/springboot/SpringbootApplication.java
@@ -2,9 +2,11 @@ package com.lvy.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableTransactionManagement
 public class SpringbootApplication {
 
