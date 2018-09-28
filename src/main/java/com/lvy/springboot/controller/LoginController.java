457b7500efeceb42389c4e0b20c6d60d3bece02e
1107343728 <1107343728@qq.com>
2018-09-28 17:26:11 +0800

    modify

--- src/main/java/com/lvy/springboot/controller/LoginController.java
+++ src/main/java/com/lvy/springboot/controller/LoginController.java
@@ -1,16 +1,17 @@
 package com.lvy.springboot.controller;
 
 import com.lvy.springboot.entity.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/rest/login")
 public class LoginController {
     @GetMapping("/inex")
     public String index() {
         System.out.println("test...........");
-        return new User().toString();
+        return "index";
     }
 }
